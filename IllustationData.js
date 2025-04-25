6408b4d9f45fc2a72fb6029154a2d382d6ca21a0
Rita Zhang <ritazhang@RitadeMacBook-Pro.local>
2025-04-25 15:13:52 +0800

    Initial commit

---
+++ IllustationData.js
@@ -0,0 +1,51 @@
+const illustrationProjects = {
+    1: {
+      title: "austria-ebbs-town",
+      image: "images/austria-ebbs-town.jpg",
+      description: "austria-ebbs-town",
+      category: "landscape"
+    },
+    2: {
+      title: "missy",
+      image: "images/missy.jpg",
+      description: "missy",
+      category: "animals"
+    },
+    3: {
+      title: "sunsun-christmas",
+      image: "images/sunsun-christmas.jpg",
+      description: "sunsun-christmas",
+      category: "animals"
+    },
+    4: {
+        title: "sunsun-delivergift",
+        image: "images/sunsun-delivergift.jpg",
+        description: "sunsun-delivergift",
+        category: "animals"
+      },
+      5: {
+        title: "sunsunandfriends",
+        image: "images/sunsunandfriends.jpg",
+        description: "sunsunandfriends",
+        category: "animals"
+      },
+      6: {
+        title: "daanpark1",
+        image: "images/daanpark1.jpg",
+        description: "daanpark1",
+        category: "landscape"
+      },
+      7: {
+        title:"daanpark2",
+        image:"images/daanpark2.jpg",
+        description: "daanpark2",
+        category: "landscape"
+      },
+      8: {
+        title:"quiet-garden",
+        image:"images/quiet-garden.jpg",
+        description: "quiet-garden",
+        category: "nature"
+      }
+  }
+  
